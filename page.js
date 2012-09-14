15a010d49db8ed34b1741cfe7f335a71f747eecb
wangjun <wangjun@witmob.com>
2012-09-14 15:07:50 +0800

    add

--- page.js
+++ page.js
@@ -5,11 +5,9 @@
  * Time: 上午11:21
  * To change this template use File | Settings | File Templates.
  */
-$(document).ready(function (){
-
+$(document).ready(function () {
     var pages = $('#article');
-    var a = document.getElementById("article").style;
-    pages.currentIndex=0;
+    pages.currentIndex = 0;
     var moveX = 0;
     $('#viewWebkit').on(
         'touchstart touchmove touchend',
@@ -20,28 +18,30 @@ $(document).ready(function (){
                 pages.moveX = 0;
                 pages.velocity = 0;
                 pages.lastX = e.originalEvent.targetTouches[0].pageX;
-                pages.touchId=e.originalEvent.targetTouches[0].identifier;
+                pages.touchId = e.originalEvent.targetTouches[0].identifier;
             }
             if (e.type == 'touchmove') {
                 pages.velocity = e.originalEvent.targetTouches[0].pageX
                     - pages.lastX;
                 pages.moveX += e.originalEvent.targetTouches[0].pageX
                     - pages.lastX;
-                moveX+= e.originalEvent.targetTouches[0].pageX
+                moveX += e.originalEvent.targetTouches[0].pageX
                     - pages.lastX;
                 pages.css({
                     '-wbkit-transform-style':'preserve-3d',
-                    '-webkit-transform' : 'translate3d(' + moveX
+                    '-webkit-transform':'translate3d(' + moveX
                         + 'px,0px,0)'
                 });
                 pages.lastX = e.originalEvent.targetTouches[0].pageX;
             }
             if (e.type == 'touchend') {
-                if (Math.abs(pages.moveX) > 1024 / 3 || Math.abs(pages.velocity)>2) {
+                if (Math.abs(pages.moveX) > 1024 / 3 || Math.abs(pages.velocity) > 2) {
+                    console.log( 1024 * (pages.currentIndex+1));
                     if (pages.moveX < 0) {
-                        console.log($('#article').width());
-//                        setTimeout('funA()', 10);
-                        pages.currentIndex++;
+                        if(1024 * (pages.currentIndex+1)<pages.get(0).scrollWidth)
+                        {
+                            pages.currentIndex++;
+                        }
                     } else {
                         if (pages.currentIndex > 0) {
                             pages.currentIndex--;
@@ -52,25 +52,19 @@ $(document).ready(function (){
                         pages.currentIndex--;
                     }
                 }
-                moveX =1024 * -(pages.currentIndex);
+                moveX = 1024 * -(pages.currentIndex);
                 pages.css(
                     {
-                        '-webkit-transform' : 'translate3d(' + moveX
+                        '-webkit-transform':'translate3d(' + moveX
                             + 'px,0px,0)',
-                        '-webkit-transition-duration' : '0.5s'
+                        '-webkit-transition-duration':'0.5s'
                     });
             }
 
         });
-    pages.on('webkitTransitionEnd', function(e) {
+    pages.on('webkitTransitionEnd', function (e) {
         pages.css({
-            '-webkit-transition-duration' : '0s'
+            '-webkit-transition-duration':'0s'
         });
     });
-
-
 });
-
-//function funA(){
-//    console.log($('#article').width());
-//}
